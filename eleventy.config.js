53b7a1bc9e595d9727e719cdd2d12603e67bd458
jujcic <janox233@gmail.com>
2026-02-11 20:33:42 +0100

    Add robots.txt and passthrough for eleventy

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles.css");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addCollection("posts", function (collectionApi) {
     return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
   });
